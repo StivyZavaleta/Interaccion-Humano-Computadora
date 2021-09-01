3abb4ee928c640b4d4ed7c6fccc7d694e0704cb2
Stivy Zavaleta <a1263277@uabc.edu.mx>
2021-09-01 15:51:05 -0700

    Problema 1 de Codigo

--- Practicas/Assets/Scripts/Problema1.cs
+++ Practicas/Assets/Scripts/Problema1.cs
@@ -7,7 +7,19 @@ public class Problema1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int[] nums = new int[]{8, 1, 2, 4, 6};
+
+        int[] result = ItemsLessThanElements(nums);
+
+        foreach(var item in result)
+        {
+            object p = Debug.Log(item);
+        }
+    }
+
+    private int[] ItemsLessThanElements(int[] nums)
+    {
+        return nums; 
     }
 
     // Update is called once per frame
